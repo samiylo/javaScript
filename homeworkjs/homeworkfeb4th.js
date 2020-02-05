d1f7541d3d812b840e3cb71c2b2fd3946875ead0
Samiylo <skryshu@gmail.com>
2020-02-05 08:57:20 -0600

    Qwestion 3

--- homeworkjs/homeworkfeb4th.js
+++ homeworkjs/homeworkfeb4th.js
@@ -28,3 +28,23 @@ function tipAmount (bill, service) {
 }
 
 tipAmount(100, "good")
+
+// Qwestion 3
+
+function totalAmount (bill, service) {
+    if (service == "good") {
+        let tip = bill / 20;
+        return tip + bill;
+    }
+
+    else if (service == "fair") {
+        let tip = bill / 15;
+        return tip + bill;
+    }
+
+    else if (service == "bad") {
+        let tip = bill / 10;
+        return tip + bill;
+
+    }
+}
