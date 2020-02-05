4eb81282a82d427794d7eeec2827996b66353524
Samiylo <skryshu@gmail.com>
2020-02-05 07:46:39 -0600

    First Qwestion

---
+++ homeworkjs/homeworkfeb4th.js
@@ -0,0 +1,8 @@
+// Qwestion 1
+
+function madlib (name, subject) {
+    var newString = name + "'s favorite subject in school is " + subject + ".";
+    return newString
+}
+
+console.log(madlib("Samiylo", "Science"))
