1716b234de608842b9a608385b9012bbda982ef3
Samiylo <skryshu@gmail.com>
2020-02-05 08:06:02 -0600

    Qwestion 2

--- homeworkjs/homeworkfeb4th.js
+++ homeworkjs/homeworkfeb4th.js
@@ -6,3 +6,25 @@ function madlib (name, subject) {
 }
 
 console.log(madlib("Samiylo", "Science"))
+
+// Qwestion 2
+
+function tipAmount (bill, service) {
+    if (service == "good") {
+        let tip = bill / 20;
+        return tip;
+    }
+
+    else if (service == "fair") {
+        let tip = bill / 15;
+        return tip;
+    }
+
+    else if (service == "bad") {
+        let tip = bill / 10;
+        return tip;
+
+    }
+}
+
+tipAmount(100, "good")
