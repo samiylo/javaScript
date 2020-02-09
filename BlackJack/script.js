45feb60da07091f0ce2db18b55a24d98f47ee26e
Samiylo <skryshu@gmail.com>
2020-02-09 10:54:48 -0600

    Update

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -3,13 +3,24 @@ let cards = [
 
 ]
 
-function createCard (cardName, cardNumber, cardFace, cardValue) {
-    let cardInfo {
-        Name: cardName,
+function createCard (cardNumber, cardSuit, cardValue) {
+    let cardInfo = {
         Number: cardNumber,
-        Face: cardFace,
-        Value: cardValue
+        Face: cardSuit,
+        Value: cardValue,
     }
 
-    createCard.append(cardInfo)
+    cards.push(cardInfo)
 }
+createCard("A", "Hearts", 1);
+createCard("A", "Spades", 1);
+createCard("A", "Clovers", 1);
+createCard("A", "Diamonds", 1);
+
+createCard("2", "Hearts", 2);
+createCard("2", "Spades", 2);
+createCard("2", "Clovers", 2);
+createCard("2", "Diamons", 2);
+
+
+console.log(cards)
