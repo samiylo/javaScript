c05e52223352162d785a4f343eea5f77036d5a48
Samiylo <skryshu@gmail.com>
2020-02-09 10:37:44 -0600

    Function Created

---
+++ BlackJack/script.js
@@ -0,0 +1,15 @@
+let cards = [
+
+
+]
+
+function createCard (cardName, cardNumber, cardFace, cardValue) {
+    let cardInfo {
+        Name: cardName,
+        Number: cardNumber,
+        Face: cardFace,
+        Value: cardValue
+    }
+
+    createCard.append(cardInfo)
+}
